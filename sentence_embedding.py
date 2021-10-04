e7ea3e7620eef177a106a90e4b37eaf9ed5a6b81
Elocinacademia <nicolezhanacademia@gmail.com>
2021-10-04 23:25:22 +0100

    4/10

--- sentence_embedding.py
+++ sentence_embedding.py
@@ -63,55 +63,79 @@ def cosine(u, v):
     return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
 
+# '''
+# ---------------------------------------------------------------------------------------------------------------------------------
+# Doc2Vec
+# Step 1:
+# We will use Gensim to show an example of how to use Doc2Vec. 
+# Further, we have already had a list of sentences. 
+# We will first import the model and other libraries and then we will build a tagged sentence corpus. 
+# Each sentence is now represented as a TaggedDocument containing a list of the words in it and a tag associated with it.
+# '''
+
+# from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+# tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(tokenized_sent)]
+
+# print(tagged_data)
+
+# '''
+# We then train the model with the parameters:
+# '''
+# model = Doc2Vec(tagged_data, vector_size = 20, window = 2, min_count = 1, epochs = 100)
+
+# '''
+# vector_size = Dimensionality of the feature vectors.
+# window = The maximum distance between the current and predicted word within a sentence.
+# min_count = Ignores all words with total frequency lower than this.
+# alpha = The initial learning rate.
+# '''
+
+# ## Print model vocabulary
+# # model.wv.vocab
+# # model.wv.key_to_index
+
+
+
+# '''
+# Step 3:
+# We now take up a new test sentence and find the top 5 most similar sentences from our data. 
+# We will also display them in order of decreasing similarity. 
+# The infer_vector method returns the vectorized form of the test sentence(including the paragraph vector). 
+# The most_similar method returns similar sentences
+# '''
+
+# test_doc = word_tokenize("sugar reading".lower())
+# test_doc_vector = model.infer_vector(test_doc)
+# # model.docvecs.most_similar(positive = [test_doc_vector])
+# kk = model.dv.most_similar(positive = [test_doc_vector])
+
+
+# ###positive = List of sentences that contribute positively.
+
+# ---------------------------------------------------------------------------------------------------------------------------------
+
 '''
-Doc2Vec
-Step 1:
-We will use Gensim to show an example of how to use Doc2Vec. 
-Further, we have already had a list of sentences. 
-We will first import the model and other libraries and then we will build a tagged sentence corpus. 
-Each sentence is now represented as a TaggedDocument containing a list of the words in it and a tag associated with it.
+BERT
+We will then load the pre-trained BERT model. 
+However, there are many other pre-trained models available. 
 '''
 
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(tokenized_sent)]
 
-print(tagged_data)
 
-'''
-We then train the model with the parameters:
-'''
-model = Doc2Vec(tagged_data, vector_size = 20, window = 2, min_count = 1, epochs = 100)
+from sentence_transformers import SentenceTransformer
+sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
 
 '''
-vector_size = Dimensionality of the feature vectors.
-window = The maximum distance between the current and predicted word within a sentence.
-min_count = Ignores all words with total frequency lower than this.
-alpha = The initial learning rate.
+Step 2:
+We will then encode the provided sentences. 
+We can also display the sentence vectors(just uncomment the code below)
 '''
+import pdb; pdb.set_trace()
 
-## Print model vocabulary
-# model.wv.vocab
-# model.wv.key_to_index
+sentence_embeddings = model.encode(sentences)
 
-
-
-'''
-Step 3:
-We now take up a new test sentence and find the top 5 most similar sentences from our data. 
-We will also display them in order of decreasing similarity. 
-The infer_vector method returns the vectorized form of the test sentence(including the paragraph vector). 
-The most_similar method returns similar sentences
-'''
-
-test_doc = word_tokenize("sugar reading".lower())
-test_doc_vector = model.infer_vector(test_doc)
-# model.docvecs.most_similar(positive = [test_doc_vector])
-kk = model.dv.most_similar(positive = [test_doc_vector])
-
-
-# positive = List of sentences that contribute positively.
-
-
+#print('Sample BERT embedding vector - length', len(sentence_embeddings[0]))
+#print('Sample BERT embedding vector - note includes negative values', sentence_embeddings[0])
 
 
 import pdb; pdb.set_trace()
